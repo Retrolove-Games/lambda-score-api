ee558b1c0cf803ddf023e670103a1a32b2e60c5c
Konrad Fedorczyk <contact@realhe.ro>
2021-04-01 19:54:52 +0200

    Add fully working example

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const ApiBuilder = require("claudia-api-builder");
 const MySql = require("sync-mysql");
 const CryptoJS = require("crypto-js");
+const SqlString = require("sqlstring");
 
-var api = new ApiBuilder();
+var api = new ApiBuilder(),
+  connection = new MySql({
+    host: process.env.RDS_HOST,
+    user: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB,
+  });
 
+/**
+ * Hello page
+ */
 api.get(
   "/",
   function (request) {
@@ -16,6 +26,37 @@ api.get(
     );
   },
   { success: 200 }
+);
+
+/**
+ * Insert score
+ */
+api.get(
+  "/score/{name}",
+  function (request) {
+    "use strict";
+
+    var projectSql = SqlString.format('SELECT * FROM projects WHERE name = ?', [request.pathParams.name]);
+
+    return connection.query(projectSql);
+  },
+  {
+    success: { code: 200, contentType: "application/json" },
+  }
+);
+
+/**
+ * Get all projects
+ */
+api.get(
+  "/projects",
+  function (request) {
+    "use strict";
+    return connection.query("SELECT name FROM projects");
+  },
+  {
+    success: { code: 200, contentType: "application/json" },
+  }
 );
 
 module.exports = api;
